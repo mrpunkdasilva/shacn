97bdf4818a0bc36391e936cad98bf54fceffdaa8
Mr Punk da Silva <gustavojs417@gmail.com>
2025-08-27 20:09:58 -0300

    feat: add dropdown menu on avatar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,53 @@
-import {Moon} from "lucide-react";
+import {LogOut, Moon, Settings, User} from "lucide-react";
 import Link from "next/link";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export const Navbar = () => {
-    return (
-        <nav className={"flex items-center justify-between p-4"}>
-            {/*LEFT*/}
-            collapseButton
+  return (
+    <nav className={"flex items-center justify-between p-4"}>
+      {/*LEFT*/}
+      collapseButton
 
-            {/*Right*/}
-            <div className="flex items-center gap-4">
-                <Link href={"/"}>Dashboard</Link>
-                <Moon/>
-            </div>
-        </nav>
-    )
+      {/*Right*/}
+      <div className="flex items-center gap-4">
+        <Link href={"/"}>Dashboard</Link>
+
+        <Moon/>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="https://avatars.githubusercontent.com/u/70487766?v=4"/>
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent sideOffset={15}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+
+            <DropdownMenuSeparator/>
+
+            <DropdownMenuItem>
+              <User className={"h-[1.2rem] w-[1.2rem] mr-2"}/> Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings className={"h-[1.2rem] w-[1.2rem] mr-2"}/> Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <LogOut className={"h-[1.2rem] w-[1.2rem] mr-2"}/> Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+      </div>
+    </nav>
+  )
 }
