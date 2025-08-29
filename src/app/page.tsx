5ee980fc40b85c8a9b1c7125e0ce417758013f32
Mr Punk da Silva <gustavojs417@gmail.com>
2025-08-29 19:41:53 -0300

    feat(components): add cardlist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
 import {AppBarChart, AppAreaChart, AppPieChart} from "@/components/charts";
+import {CardList} from "@/components/CardList";
 
 export default function HomePage() {
   return (
-   <div className={"grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4"}>
-     <div className="bg-primary-foreground p-4 rounted-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-       <AppBarChart />
-     </div>
-     <div className="bg-primary-foreground p-4 rounted-lg">Test</div>
-     <div className="bg-primary-foreground p-4 rounted-lg">
-       <AppPieChart />
-     </div>
-     <div className="bg-primary-foreground p-4 rounted-lg">Test</div>
-     <div className="bg-primary-foreground p-4 rounted-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-       <AppAreaChart/>
-     </div>
-     <div className="bg-primary-foreground p-4 rounted-lg">Test</div>
-   </div>
+    <div className={"grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4"}>
+      <div className="bg-primary-foreground p-4 rounted-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart/>
+      </div>
+      <div className="bg-primary-foreground p-4 rounted-lg">
+        <CardList title="Latest Transactions"/>
+      </div>
+      <div className="bg-primary-foreground p-4 rounted-lg">
+        <AppPieChart/>
+      </div>
+      <div className="bg-primary-foreground p-4 rounted-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounted-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart/>
+      </div>
+      <div className="bg-primary-foreground p-4 rounted-lg">Test</div>
+    </div>
   );
 }
---
+++ src/components/CardList.tsx
@@ -0,0 +1,33 @@
+import {LATEST_TRANSACTIONS, POPULAR_CONTENT} from "@/components/card-list";
+import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import Image from "next/image";
+import {Badge} from "lucide-react";
+
+export function CardList({title}: { title: string }) {
+  const list = title === "Popular Content" ? POPULAR_CONTENT : LATEST_TRANSACTIONS;
+
+  return (
+      <div className="">
+      <h1 className="text-lg font-medium mb-6">{title}</h1>
+      <div className="flex flex-col gap-2">
+        {list.map((item) => (
+          <Card key={item.id} className="flex-row items-center justify-between gap-4 p-4">
+            <div className="w-12 h-12 rounded-sm relative overflow-hidden">
+              <Image
+                src={item.image}
+                alt={item.title}
+                fill
+                className="object-cover"
+              />
+            </div>
+            <CardContent className="flex-1 p-0">
+              <CardTitle className="text-sm font-medium">{item.title}</CardTitle>
+              <Badge fontVariant="secondary">{item.badge}</Badge>
+            </CardContent>
+            <CardFooter className="p-0">{item.count / 1000}K</CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
