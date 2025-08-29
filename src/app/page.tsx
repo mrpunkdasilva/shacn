803b027b5dd5cd3c9908c3447e949f0ec00a6df3
Mr Punk da Silva <gustavojs417@gmail.com>
2025-08-29 17:49:07 -0300

    feat(components): add grid system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 
 export default function HomePage() {
   return (
-   <div className={""}>
-
+   <div className={"grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4"}>
+     <div className="bg-primary-foreground p-4 rounted-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test</div>
+     <div className="bg-primary-foreground p-4 rounted-lg">Test</div>
+     <div className="bg-primary-foreground p-4 rounted-lg">Test</div>
+     <div className="bg-primary-foreground p-4 rounted-lg">Test</div>
+     <div className="bg-primary-foreground p-4 rounted-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test</div>
+     <div className="bg-primary-foreground p-4 rounted-lg">Test</div>
    </div>
   );
 }
